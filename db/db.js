645da62352e24bd0013ae02fb49dfa49a0ebd446
GUILHERME DE ALMEIDA CRUZ <GUILHERME0996@DIGITALCOLLEGE.LOCAL>
2023-12-21 19:16:11 -0300

    configurando o sequelise com stirng de conexao

---
+++ db/db.js
@@ -0,0 +1,30 @@
+import { Sequelize } from "sequelize";
+import { DB_CONFIG } from "./config/config.js";
+
+Const URL_RENDER = ${DB_CONFIG.DIALECT}:$ {DB_CONFIG.USER}:${DB_CONFIG.PASS}
+console.log
+export const connection = new Sequelize(
+  DB_CONFIG.db,
+  DB_CONFIG.user,
+  DB_CONFIG.pass,
+  {
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false, // Configuração para evitar o erro "SSL/TLS required"
+      },
+    },
+    pool: {
+      max: 5, // maximo de conexoes suportados 
+      min: 0, // minimo de conexoes suportados
+      acquire: 30000, // tempo maximo, em ms(milisegundos) que o pool tenta conectar antes de gerar o erro
+      idle: 10000, // tempo maximo, em ms(milisegundos) que o pool pode ser confirmado antes de ser estabelecido
+    }
+  });
+
+try {
+  await connection.authenticate();
+  console.log('Connection has been established successfully.');
+} catch (error) {
+  console.error('Unable to connect to the database:', error);
+}
